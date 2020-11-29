d92900d3834e64c3cdb1265d9d3358b1a1941062
Pantelis Kassotis <pantelis.kassotis@gmail.com>
2020-11-29 13:10:27 +0200

    Game.py

--- Game.py
+++ Game.py
@@ -2,8 +2,18 @@ print("The more of this there is, the less you see. What is it?")
 
 secret_word = "darkness"
 guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
 
-while guess != secret_word:
-    guess = input("Enter your guess: ")
+while guess != secret_word and not (out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter your guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
 
-print("You win!")
+if out_of_guesses:
+    print("GAME OVER")
+else:
+    print("You win!")
